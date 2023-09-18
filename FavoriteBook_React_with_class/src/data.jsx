1342af78c190e549a80fcbdda74b27446d21596b
Shems <96719853+IsShems@users.noreply.github.com>
2023-09-18 22:23:53 +0400

    Update data.jsx

--- FavoriteBook_React_with_class/src/data.jsx
+++ FavoriteBook_React_with_class/src/data.jsx
@@ -1,13 +1,13 @@
 // data.jsx
 const BookData = {
-  title: "Murder Mystery",
-  author: "Leyla Guseynli Mamedov",
-  genre: "Detective",
-  pages: 342,
+  title: "The Little Prince",
+  author: "Antoine de Saint-Exupéry",
+  genre: "Novella",
+  pages: 251,
   reviews: [
     "Great book!",
-    "The plot is captivating from the first pages.",
-    "I recommend it to all genre enthusiasts."
+    "The most meaningful book you must read even if you are a fully grown up. This could give you so many questions regarding relations with your friends or family. How you could cherish what you have free from greed we all always have had deep inside. Once you touch this book and start to flip the pages, you cant stop yourself from thinking of yourself in every possible way :) So amazing work Sanitexupery made for the future generations. Please read it and make yourself better !",
+    "One of the most heartwarming and extraordinary books you'll ever read. It's a short but sweet book, and I couldn't put it down (finished in 1h:30). I found myself actually crying when I finished, which isn't something I do with sad media, but I genuinely needed to hear the message behind this book. I am very glad I read The Little Prince.The most beautiful things in the world cannot be seen or touched, they are felt with the heart."
   ],
   };
   
