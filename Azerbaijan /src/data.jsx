6d95977edbbd3e9b779dd9be506f06c69f85d3a1
Shems <96719853+IsShems@users.noreply.github.com>
2023-09-18 20:53:46 +0400

    Create data.jsx

---
+++ Azerbaijan /src/data.jsx
@@ -0,0 +1,21 @@
+// data.js
+const cityData = {
+    name: "Baku",
+    country: "Azerbaijan",
+    foundationYear: 861, 
+    landmarks: [
+      "İçeri Şəhər",
+      "Heydar Aliyev Center",
+      "Baku Olympic Stadium",
+      "Flame Towers"
+    ],
+    ims:[
+      require("./images/caption.jpg"),
+      require("./images/Heydar-Aliyev-Center.webp"),
+      require("./images/5b2e715454659.jpg"),
+      require("./images/download.jpg")
+    ]
+  };
+  
+  export default cityData;
+  
