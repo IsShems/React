53cc347e9453102cd65ab93c88f40b35dc06bd04
Shems <96719853+IsShems@users.noreply.github.com>
2023-09-18 22:17:55 +0400

    Create App.js

---
+++ FavoriteBook/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import BookInfo from "./BookInfo";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BookInfo />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
