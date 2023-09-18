c6b48acf28a6c6aeb58b8a4bd6e3562b1f648286
Shems <96719853+IsShems@users.noreply.github.com>
2023-09-18 20:48:01 +0400

    Create App.js

---
+++ Azerbaijan /src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import CityInfo from "./CityInfo";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CityInfo />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
