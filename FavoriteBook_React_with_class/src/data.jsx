4d4f100d9882971c5264e8450e1fe199d985fff0
Shems <96719853+IsShems@users.noreply.github.com>
2023-09-18 22:00:05 +0400

    Create data.jsx

---
+++ FavoriteBook_React_with_class/src/data.jsx
@@ -0,0 +1,15 @@
+// data.jsx
+const BookData = {
+  title: "Murder Mystery",
+  author: "Leyla Guseynli Mamedov",
+  genre: "Detective",
+  pages: 342,
+  reviews: [
+    "Great book!",
+    "The plot is captivating from the first pages.",
+    "I recommend it to all genre enthusiasts."
+  ],
+  };
+  
+  export default BookData;
+  
