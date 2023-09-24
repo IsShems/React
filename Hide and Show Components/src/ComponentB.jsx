ab41e0c2ddcd8b9fb155098254142d56ec4dc2e5
Shems <96719853+IsShems@users.noreply.github.com>
2023-09-24 14:15:39 +0400

    Create ComponentB.jsx

---
+++ Hide and Show Components/src/ComponentB.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ComponentB = () => {
+  return (
+    <div>
+      <h2>B</h2>
+      <p>This is component B</p>
+    </div>
+  );
+};
+
+export default ComponentB;
