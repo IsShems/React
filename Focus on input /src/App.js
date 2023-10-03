d84a7145b147e14b15317860da6b19b2ba5e62c8
Shems <96719853+IsShems@users.noreply.github.com>
2023-10-03 21:37:14 +0400

    Update App.js

--- Focus on input /src/App.js
+++ Focus on input /src/App.js
@@ -1,1 +1,38 @@
 
+import React, { useRef, useEffect, useState } from 'react';
+
+function FocusInput() {
+  const inputRef = useRef(null);
+  const [shouldFocus, setShouldFocus] = useState(false);
+
+  const focusInput = () => {
+    setShouldFocus(true);
+  };
+
+  useEffect(() => {
+    if (shouldFocus) {
+      const inputElement = inputRef.current;
+      if (inputElement) {
+        inputElement.focus();
+      }
+      // Для сбрсавыния флага shouldFocus
+      //setShouldFocus(false);
+    }
+  }, [shouldFocus]);
+
+  return (
+    <div>
+      <button onClick={focusInput}>Установить фокус</button>
+      <input
+        type="text"
+        ref={inputRef}
+        placeholder={shouldFocus ? 'Фокус установлен' : 'Нажмите кнопку для установки фокуса'}
+      />
+      <p> "Фокус установлен" сработает при нажатии, также можно чтобы сбрасывался фокус автоматически для этого раскомментируйте //setShouldFocus(false); в коде</p>
+      <button onClick={() => setShouldFocus(false)}>Сбросить фокус</button>
+      
+    </div>
+  );
+}
+
+export default FocusInput;
