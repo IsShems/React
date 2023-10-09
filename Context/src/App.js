ee1749a730a84ecb3ff354a8a473b48dfcd88283
Shems <96719853+IsShems@users.noreply.github.com>
2023-10-09 16:54:34 +0400

    Update App.js

--- Context/src/App.js
+++ Context/src/App.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { ThemeProvider } from './theme/ThemeContext';
+import ThemeToggle from './theme/ThemeToggle';
+import ThemedComponent from './theme/ThemedComponent'; 
+import './App.css';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <div className="App">
+        <h1>Theme Switcher App</h1>
+        <ThemeToggle />
+        <ThemedComponent /> {}
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
 
