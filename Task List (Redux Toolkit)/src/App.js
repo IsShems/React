1a23e9913b6bcddb5a4b57cf9bd96454f03c6a30
Shems <96719853+IsShems@users.noreply.github.com>
2023-10-10 11:44:38 +0400

    Update App.js

--- Task List (Redux Toolkit)/src/App.js
+++ Task List (Redux Toolkit)/src/App.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import TaskList from './TaskList';
+import TaskForm from './TaskForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Task List</h1>
+      <TaskForm/>
+      <TaskList/>
+    </div>
+  );
+}
+
+export default App;
 
