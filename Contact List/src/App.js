ec0349e68425c5a8392381f3be6d81eeaea6561a
Shems <96719853+IsShems@users.noreply.github.com>
2023-10-10 11:17:58 +0400

    Update App.js

--- Contact List/src/App.js
+++ Contact List/src/App.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import ContactList from './ContactList';
+import ContactForm from './ContactForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Contact List</h1>
+      <ContactForm />
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
 
