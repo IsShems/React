ccef38020a6d173424649d6fbf632fc07ae577e1
Shems <96719853+IsShems@users.noreply.github.com>
2023-10-24 12:44:37 +0400

    Create Header.js

---
+++ NewsApp/src/Header.js
@@ -0,0 +1,30 @@
+import './Header.css';
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleSortOrder  } from './store/reducer'
+import { React } from 'react';
+
+export function Header({ darkMode, setDarkMode  }) {
+
+  const sortOrder = useSelector((state) => state.news.sortOrder);
+  let dispatch = useDispatch()
+
+  const toggleSortOrderHandle = () => {
+    dispatch(toggleSortOrder());
+  }
+
+  const changeTheme = () => {
+      setDarkMode(!darkMode);
+    }
+  
+    return (
+      <div className='header-container'>
+        <button className='sortButton' onClick={toggleSortOrderHandle}>
+          {sortOrder === 'ASC' ? 'Oldest to Newest ▼' : 'Newest to Oldest ▲'}
+        </button>
+        <button className='themeButton' onClick={changeTheme}>{darkMode ? '🌙' : '🔅' }</button>
+      </div>
+    );
+  }
+  
+  export default Header;
