31dbc6d200887d5e5cb1f1d368bdc0677ea712ee
Shems <96719853+IsShems@users.noreply.github.com>
2023-10-24 12:40:18 +0400

    Create NewsList.jsx

---
+++ NewsApp/src/NewsList.jsx
@@ -0,0 +1,38 @@
+import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export function NewsList({ newsArr }) {
+    return (
+        <div className='app-container' >
+            {Array.isArray(newsArr) && newsArr.length > 0 ? (
+                newsArr.map((article, index) => (
+                    <Link to={`details/${index}`} key={index}>
+                        <div>
+                            <h1>{article.title}</h1>
+                            <div style={{ display: 'flex' }}>
+                                <Image src={article.urlToImage} alt={article.title} />
+                                <div>
+                                    <div>
+                                        <h2>{article.author}</h2>
+                                    </div>
+                                    <p>Published at: {article.publishedAt}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                ))
+            ) : (
+                <p>No articles available.</p>
+            )}
+        </div>
+    );
+}
+
+const Image = styled.img`
+    width: 300px;
+    height: 200px;
+`;
+
+export default NewsList;
