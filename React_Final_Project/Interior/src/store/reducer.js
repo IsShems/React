bd457d24e0270c79f0c954a952a2dba2f5157620
Shems <96719853+IsShems@users.noreply.github.com>
2023-11-17 16:20:01 +0400

    Add files via upload

---
+++ React_Final_Project/Interior/src/store/reducer.js
@@ -0,0 +1,120 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const fetchContent = createAsyncThunk(
+  "content/fetchContent",
+  async () => {
+    var url = "/data.json";
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  }
+);
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState: {
+    dataArr: [],
+    basketArr: [],
+    isLoading: false,
+    error: null,
+  },
+
+  reducers: {
+    addToShoppingCart: (state, action) => {
+      const productToAdd = action.payload
+      console.log(productToAdd.id)
+      const existingProductIndex = state.basketArr.findIndex(
+        (product) => product.id === productToAdd.id
+      )
+      if (existingProductIndex !== -1) {
+        const updatedBasketArr = state.basketArr.map((product, index) => {
+          if (index === existingProductIndex) {
+            return {
+              ...product,
+              quantity: product.quantity + 1,
+            };
+          }
+          return product;
+        });
+
+        return { ...state, basketArr: updatedBasketArr }
+      } else {
+        const newProduct = { ...productToAdd, quantity: 1 }
+        const updatedBasketArr = [...state.basketArr, newProduct]
+
+        return { ...state, basketArr: updatedBasketArr }
+      }
+    },
+    increaseQuantity: (state, action) => {
+      const productToUpdate = action.payload
+      const updatedBasketArr = state.basketArr.map((product) => {
+        if (product.id === productToUpdate.id) {
+          return {
+            ...product,
+            quantity: product.quantity + 1,
+          };
+        }
+        return product
+      });
+
+      return { ...state, basketArr: updatedBasketArr }
+    },
+
+    decreaseQuantity: (state, action) => {
+      const productToUpdate = action.payload
+      const updatedBasketArr = state.basketArr.map((product) => {
+        if (product.id === productToUpdate.id) {
+          if (product.quantity > 1) {
+            return {
+              ...product,
+              quantity: product.quantity - 1,
+            }
+          }
+          else{
+            return {...product}
+          }
+        }
+        return product
+      })
+
+      return { ...state, basketArr: updatedBasketArr }
+    },
+
+    updateDataArr:(state, action) => {
+      state.dataArr = action.payload;
+    },
+
+    removeProduct: (state, action) => {
+      const productToRemove = action.payload
+      const updatedBasketArr = state.basketArr.filter(
+        (product) => product.id !== productToRemove.id
+      )
+
+      return { ...state, basketArr: updatedBasketArr }
+    },
+
+    clearBasket: (state, action) => {
+      const newArr = []
+      return {...state, basketArr: newArr}
+    }
+  
+  },
+
+  
+  extraReducers: (builder) => {
+    builder.addCase(fetchContent.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchContent.fulfilled, (state, action) => {
+      state.dataArr = action.payload;
+      state.isLoading = false;
+    });
+    builder.addCase(fetchContent.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = true;
+    });
+  },
+});
+
+export const { addToShoppingCart, increaseQuantity, decreaseQuantity, removeProduct, clearBasket, updateDataArr } = productsSlice.actions;
+export default productsSlice.reducer;
