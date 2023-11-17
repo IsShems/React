c408a7741a89d1f73f7b789851cea7f1f9a4f9b3
Shems <96719853+IsShems@users.noreply.github.com>
2023-11-17 16:18:33 +0400

    Add files via upload

--- React_Final_Project/Interior/src/App.js
+++ React_Final_Project/Interior/src/App.js
@@ -1,1 +1,20 @@
+import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import { Header } from './header/Header';
+import  Main  from './main/main';
+import { Footer } from './footer/Footer';
 
+function App() {
+  
+  return (
+    <div>
+      <BrowserRouter>
+        <Header/>
+        <Main/>
+        <Footer/>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
